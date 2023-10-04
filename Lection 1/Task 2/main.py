07e26d1b7ea1346eb50d9f22c72e94b6155aef27
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-10-04 16:11:48 +0300

    Added second task of the first lection

---
+++ Lection 1/Task 2/main.py
@@ -0,0 +1,4 @@
+print("Sum:", int(input("Enter number one\n")) + int(input("Enter number two\n")))               
+numberOne = input("Enter your first number to get a sum\n")
+numberTwo = input("Enter your second number to get a sum\n")
+print("Sum:", numberOne, "+", numberTwo, "=", int(numberOne) + int(numberTwo))
