cfecc71c2ac04a0ed3617ab23e4bdf7bdcfc701f
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-10-05 18:27:09 +0300

    updated task 4

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,9 +1,9 @@
-userAnswer = input("Say 1 - F to C or say 2 - C to F\n")
-if userAnswer == '1':
-    userF = int(input("Enter your F\n"))
-    print("F = ", userF, "\nC = ",(userF - 32)/1.8)
-elif userAnswer == '2':
-    userC = int(input("Enter your C\n"))
-    print("C = ", userC, "\nF = ", (userC * 1.8)+32)
+userAnswer , userData= input("Enter C - F to C , enter F - C to F, enter SPACE and your number to convert\n").split()
+if userAnswer == 'C':
+    #userF = int(input("Enter your F\n"))
+    print("F = ", int(userData), "\nC = ",(int(userData) - 32)/1.8)
+elif userAnswer == 'F':
+    #userC = int(input("Enter your C\n"))
+    print("C = ", int(userData), "\nF = ", (int(userData) * 1.8)+32)
 else:
     print('Wrong input')
