fa1225684409ae10610f225042779b58eb1bb3eb
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-10-14 17:36:28 +0300

    updated task 4

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,28 +1,26 @@
 import math
 def calc(data):
     result = int(data[1])
-    if "*" in data:
-        for i in data[2:]:
-            result *= int(i)
-    elif "/" in data:
-        for i in data[2:]:
-            result /= int(i)
-    elif "+" in data:
-        for i in data[2:]:
-            result += int(i)
-    elif "-" in data:
-        for i in data[2:]:
-            result -+ int(i)
-    elif "**" in data:
-        for i in data[2:]:
-            result = result**int(i)
-    elif "sqrt" in data:
-        result = []
-        for i in data[1:]:
-            print(math.sqrt(int(i)))
-            result.append(math.sqrt(int(i)))
+    match data[0]:
+        case "*":
+            for i in data[2:]:
+                result *= int(i)
+        case "/":
+            for i in data[2:]:
+                result /= int(i)
+        case "+":
+            for i in data[2:]:
+                result += int(i)
+        case "-":
+            for i in data[2:]:
+                result -= int(i)
+        case "**":
+            for i in data[2:]:
+                result =result**int(i)
+        case "sqrt":
+            result =[]
+            for i in data[1:]:
+                result.append(math.sqrt(int(i)))
     return result
-
-
-usrData=input("Choose your action and enter you numbers like: * 12 5 13\nActions: *, /, +, -, **, sqrt").split()
+usrData=input("Choose your action and enter you numbers like: * 12 5 13\nActions: *, /, +, -, **, sqrt: ").split()
 print (calc(usrData))
