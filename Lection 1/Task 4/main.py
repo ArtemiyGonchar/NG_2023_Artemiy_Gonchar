5e1c6c77c3e632871d7c28c44611b1e5bc34de36
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-10-17 21:21:58 +0300

    reworked task4

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,26 +1,32 @@
 import math
 def calc(data):
+    print (data)
     result = int(data[1])
-    match data[0]:
-        case "*":
-            for i in data[2:]:
+    action = data.pop(0)
+    print (action)
+    for i in data [2:]:
+        match data[0]:
+            case "*":
+                #for i in data[2:]:
                 result *= int(i)
-        case "/":
-            for i in data[2:]:
+            case "/":
+                #for i in data[2:]:
                 result /= int(i)
-        case "+":
-            for i in data[2:]:
+            case "+":
+                #for i in data[2:]:
                 result += int(i)
-        case "-":
-            for i in data[2:]:
+            case "-":
+                #for i in data[2:]:
                 result -= int(i)
-        case "**":
-            for i in data[2:]:
+            case "**":
+                #for i in data[2:]:
                 result =result**int(i)
-        case "sqrt":
-            result =[]
-            for i in data[1:]:
-                result.append(math.sqrt(int(i)))
+            case "sqrt":
+                result =[]
+                #for i in data[1:]:
+                for a in data[1:]:
+                    result.append(math.sqrt(int(a)))
+                break
     return result
 usrData=input("Choose your action and enter you numbers like: * 12 5 13\nActions: *, /, +, -, **, sqrt: ").split()
 print (calc(usrData))
