96f6ad5f2b71d08eb7de7d7baaba9d150df59992
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-10-17 21:24:54 +0300

    reworked task4

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,32 +1,16 @@
-import math
-def calc(data):
-    print (data)
-    result = int(data[1])
-    action = data.pop(0)
-    print (action)
-    for i in data [2:]:
-        match data[0]:
-            case "*":
-                #for i in data[2:]:
-                result *= int(i)
-            case "/":
-                #for i in data[2:]:
-                result /= int(i)
-            case "+":
-                #for i in data[2:]:
-                result += int(i)
-            case "-":
-                #for i in data[2:]:
-                result -= int(i)
-            case "**":
-                #for i in data[2:]:
-                result =result**int(i)
-            case "sqrt":
-                result =[]
-                #for i in data[1:]:
-                for a in data[1:]:
-                    result.append(math.sqrt(int(a)))
-                break
-    return result
-usrData=input("Choose your action and enter you numbers like: * 12 5 13\nActions: *, /, +, -, **, sqrt: ").split()
-print (calc(usrData))
+def calc(action,un1,un2):
+    match action:
+        case "*":
+            return un1*un2
+        case "/":
+            return un1/un2
+        case "+":
+            return un1+un2
+        case "-":
+            return un1-un2
+        case "**":
+            un1**un2
+        case "sqrt":
+            return (un1 ** 0.5), (un2 ** 0.5)
+
+print (calc(action = input("First, choose your action and then enter your two numbers\nActions: *, /, +, -, **, sqrt: "), un1 = int(input("First number: ")), un2 = int(input("Second number: "))))
