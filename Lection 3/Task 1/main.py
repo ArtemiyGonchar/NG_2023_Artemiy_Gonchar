d6b7791597d58f3ce42ead31ec8a7c5b0f76e573
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-10-27 13:56:31 +0300

    deleted one cycle for

--- Lection 3/Task 1/main.py
+++ Lection 3/Task 1/main.py
@@ -1,7 +1,6 @@
 usrf = input("Enter file to open: ")
 unicval = {}
 with open(usrf, "r") as filehandle:
-    for lines in filehandle:
-        for charac in lines:
-            unicval[charac] = unicval.get(charac, 0) + 1
+    for charac in filehandle.read():
+        unicval[charac] = unicval.get(charac, 0) + 1
 print (unicval)
