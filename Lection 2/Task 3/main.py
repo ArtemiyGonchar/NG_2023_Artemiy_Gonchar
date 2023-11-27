f0ab7a5a3a1aec5f9679b5f3ccd9aed9a2e20bbd
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-11-27 19:16:37 +0200

    full reworked task

--- Lection 2/Task 3/main.py
+++ Lection 2/Task 3/main.py
@@ -1,15 +1,12 @@
-number = int(input("Enter your number: "))
-numlst = []
-for i in range(1, number + 1):
-    divisions = None
-    if (i % 2 == 0) and (i % 3 == 0):
-        print("{}  | 2, 3".format(i))
-    elif i % 2 == 0:
-        print("{}  | 2".format(i))
-    elif i % 3 == 0:
-        print("{}  | 3".format(i))
-    else: 
-        print("{}  | ".format(i))
-        numlst.append(i)
-    print('---')
-print (numlst)
+startNumber = int(input('Enter your number: '))
+primeNumbers = []
+print("Numbers\t| Divisors")
+for number in range(1, startNumber + 1):
+    divisionNumbers = []
+    for division in range(1, number + 1):
+        if (number % division == 0):
+            divisionNumbers.append(division)
+    if len(divisionNumbers) == 2 or len(divisionNumbers) == 1:
+        primeNumbers.append(number)
+    print("{}\t| {}".format(number, divisionNumbers))
+print("\nPrime numbers:\n",primeNumbers)
