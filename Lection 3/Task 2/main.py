2c0e7bc2b3a083a2454b0ab297afe42c4a69910e
Artemiy Gonchar <artmiygonchar@gmail.com>
2023-11-29 00:08:05 +0200

    reworked OS Version getting

--- Lection 3/Task 2/main.py
+++ Lection 3/Task 2/main.py
@@ -5,7 +5,8 @@ computerSpecs = {}
 computerSpecs["Processor"] = ((cpuinfo.get_cpu_info())['brand_raw'])
 computerSpecs["Number of CPUs"] = multiprocessing.cpu_count()
 computerSpecs["Operating System"] = platform.platform()
-computerSpecs["OS Version"] = platform.version().split()[3]+' '+platform.version().split()[4]
+#computerSpecs["OS Version"] = platform.version().split()[3]+' '+platform.version().split()[4]
+computerSpecs["OS Version"] = platform.version() + platform.release()
 computerSpecs["Computer Name"] = platform.node()
 computerSpecs["RAM"] = str((os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES'))/(1024.**3)) + "GB"
 
